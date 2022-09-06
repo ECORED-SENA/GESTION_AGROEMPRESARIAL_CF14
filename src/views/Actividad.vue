d679bb2fd22f6a3451c7617437398dc642d1f6bf
SANDRATRIANA <sandrajem23@hotmail.com>
2022-09-06 18:27:45 -0500

    actividad-zip

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -35,7 +35,7 @@
               p.fw-bold.mb-0 Verdadero/falso 
               falso 
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/actividad2/actividad.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
     
